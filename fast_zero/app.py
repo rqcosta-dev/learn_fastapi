5f8c1829ac15cdb80bffe39f1af8d964ebc69477
Roberto Costa <roberto@craftysystems.com.br>
2025-02-01 19:28:41 -0300

    Add HTML response endpoint and message schema to FastAPI app

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,25 @@
+from http import HTTPStatus
 from fastapi import FastAPI
+from fast_zero.schemas import Message
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/", status_code=HTTPStatus.OK, response_model=Message)  # 200 OK
 def read_root():
     return {"message": "Hello World!"}
+
+
+@app.get("/web-page", status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def response_html():
+    return """
+    <html>
+        <head>
+            <title>FastAPI</title>
+        </head>
+        <body>
+            <h1>Hello World!</h1>
+        </body>
+    </html>
+    """
--- tests/test_app.py
+++ tests/test_app.py
@@ -14,3 +14,19 @@ def test_read_root_success():
     assert response.json() == {
         "message": "Hello World!"
     }  # Assert that the response body is {"message": "Hello World!"}
+
+
+def test_response_html_success():
+    client = TestClient(app)  # Create a TestClient instance | Arrange
+
+    response = client.get("/web-page")  # Make a GET request to the /web-page path | Act
+
+    assert (
+        response.status_code == HTTPStatus.OK
+    )  # Assert that the response status code is 200 OK
+    assert (
+        response.headers["content-type"] == "text/html; charset=utf-8"
+    )  # Assert that the response content type is text/html; charset=utf-8
+    assert (
+        "<h1>Hello World!</h1>" in response.text
+    )  # Assert that the response body contains <h1>Hello World!</h1>
