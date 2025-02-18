4ae57e8fbaf025028a5979d7e63e780a46cbf1b0
Roberto Costa <roberto@craftysystems.com.br>
2025-02-17 22:34:06 -0300

    Refactor CI pipeline and entrypoint script for improved configuration and clarity

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,16 +2,16 @@
 
 set -e
 
-if [ "$ENV" = "production" ]; then
+if [ "$ENV" = "prod" ]; then
     echo "Running database migrations..."
     poetry run alembic upgrade head
 
     echo "Starting the application in production mode..."
-    poetry run fastapi run fast_zero/app.py --host 0.0.0.0
+    poetry run fastapi run fast_zero/app.py --host 0.0.0.0 --port=8000
 else
     echo "Running database migrations..."
     poetry run alembic upgrade head
 
     echo "Starting the application in development mode..."
-    poetry run fastapi dev fast_zero/app.py --host 0.0.0.0
+    poetry run fastapi dev fast_zero/app.py --host 0.0.0.0 --port=8000
 fi
